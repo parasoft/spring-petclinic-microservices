f6879ef38e4ce92febfc818e13a14eddb0155ede
Aaron Humphrey <humphrey@parasoft.com>
2026-07-05 21:07:58 -0700

    Convert playwright test to use new parasoft coverage integration libraries

--- spring-petclinic-playwright-tests/src/test/java/org/springframework/samples/petclinic/playwright/VisitIT.java
+++ spring-petclinic-playwright-tests/src/test/java/org/springframework/samples/petclinic/playwright/VisitIT.java
@@ -1,18 +1,10 @@
 package org.springframework.samples.petclinic.playwright;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import org.springframework.samples.petclinic.testcommon.ParasoftSessionManager;
-import org.springframework.samples.petclinic.testcommon.ParasoftSettings;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
@@ -21,48 +13,35 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.AriaRole;
+import com.parasoft.coverage.integration.playwright.PlaywrightCoverageIntegration;
 
-@ExtendWith(org.springframework.samples.petclinic.testcommon.junit5.playwright.ParasoftWatcherPlaywright.class)
 public class VisitIT {
-       static Playwright playwright;
+    static Playwright playwright;
     static Browser browser;
 
     private static final String PETCLINIC_URL = System.getProperty("PETCLINIC_URL", "http://localhost:8099");
 
-    private static String playwrightSessionId;
     BrowserContext context;
     Page page;
 
     @BeforeAll
     static void launchBrowser() {
-        playwrightSessionId = UUID.randomUUID().toString();
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(ParasoftSettings.isHeadless()).setSlowMo(500));
-        if (ParasoftSettings.isParallelTestExecution() && ParasoftSettings.isMultiUserMode()) {
-            ParasoftSessionManager.registerParallelId(VisitIT.class.getName(), playwrightSessionId);
-        }
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(500));
     }
 
     @AfterAll
     static void closeBrowser() {
         browser.close();
         playwright.close();
-        ParasoftSessionManager.unregister(VisitIT.class.getName());
     }
 
     @BeforeEach
     void createContextAndPage() {
-        context = browser.newContext();
         // Using Playwright API for request header injection, required for Parasoft
         // coverage reporting when agents are in multi-user mode
-        if (ParasoftSettings.isMultiUserMode()) {
-            String baggage = ParasoftSessionManager.getBaggage(VisitIT.class.getName());
-            if (baggage != null && !baggage.isBlank()) {
-                Map<String, String> headers = new HashMap<>();
-                headers.put("baggage", baggage);
-                context.setExtraHTTPHeaders(headers);
-            }
-        }
+        Browser.NewContextOptions browserContextOptions = PlaywrightCoverageIntegration.createBrowserContextOptions();
+        context = browser.newContext(browserContextOptions);
         page = context.newPage();
     }
 
